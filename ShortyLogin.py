8d26af7c0bc9cfaea00e1199ea7da53cb7ff84b8
lkuffo <lkuffo@espol.edu.ec>
2022-04-21 11:11:54 +0200

    Some fixes and new relic

--- ShortyLogin.py
+++ ShortyLogin.py
@@ -1,25 +1,29 @@
 from datetime import datetime, timedelta
 from flask import Flask, request, redirect, render_template
-import validators
+import newrelic
 import os
 import jwt
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))   # refers to application_top
 APP_STATIC = os.path.join(APP_ROOT, 'static')
 
 app = Flask('ShortyLogin')
-USERS = { }
+USERS = {}
 
 SECRET = "DABESTTEAM"
 
 
 @app.route('/users', methods=['POST'])
 def create_one_user():
-    username = request.args.get('username')
-    password = request.args.get('password')
-    print(username, password)
     try:
+        username = request.form.get('username')
+        password = request.form.get('password')
+        print(username, password)
+        if username is None or username == '':
+            return 'Missing parameters', 400
+        if password is None or password == '':
+            return 'Missing parameters', 400
         if username in USERS:
-            return f'Username already existed', 401
+            return f'Username already exist', 409
         else:
             USERS[username] = {}
             USERS[username]['password'] = password
@@ -32,23 +36,31 @@ def create_one_user():
 
 @app.route('/users/login', methods=['POST'])
 def login():
-    username = request.args.get('username')
-    password = request.args.get('password')
+    username = request.form.get('username')
+    password = request.form.get('password')
     stamp = datetime.now() + timedelta(days=1)
     try:
         if username in USERS.keys() and password == USERS[username]['password']:
-            return "{encoded_jwt}".format(encoded_jwt=jwt.encode({"username": "{name}".format(name = username), "exp": stamp}, "DABESTTEAM", algorithm="HS256")), 200
+            return "{encoded_jwt}".format(encoded_jwt=jwt.encode({"username": "{name}".format(name = username), "exp": stamp}, SECRET, algorithm="HS256")), 200
         else:
-            return f"check the username and password", 404
+            return f"check the username and password", 403
     except Exception as e:
         print(e)
         return f'Unexpected error', 500
+
+
+@app.after_request
+def add_header(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    return response
 
 
 if __name__ == "__main__":
     from waitress import serve
     debug = True
     if debug:
-        app.run(debug=True)
+        app.run(debug=True, port=8081)
     else:
         serve(app, host="0.0.0.0", port=8081)
+
+# NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python3 ShortyLogin.py
